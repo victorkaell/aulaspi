4ab78442c23d823676326a12dc35efbc2cce3605
Victor Kaell <victorkaell13@gmail.com>
2025-09-18 16:03:55 -0300

    Adicionado verificação de dados do Evento
    Foram feitas modificações ao EventosController para imprimir os resultados do formulário preenchido e integrado as páginas para serem navegadas uma à outra.

--- src/main/java/ifrn/pi/inicio/controllers/EventosController.java
+++ src/main/java/ifrn/pi/inicio/controllers/EventosController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import ifrn.pi.inicio.models.Evento;
+
 @Controller
 public class EventosController {
 	
@@ -13,8 +15,16 @@ public class EventosController {
 	}
 	
 	@PostMapping("/eventos/adicionar")
-	public String adicionarEvento() {
+	public String adicionarEvento(Evento evento) {
 		System.out.println("Evento foi adicionado");
+		System.out.println("Nome: " + evento.getNome());
+		System.out.println("Local: " + evento.getLocal());
+		System.out.println("Data: " + evento.getData());
+		System.out.println("Horário: " + evento.getHorario());
+		
+		// Ao realizar o passo 1, o evento foi registrado com a devida confirmação e os dados foram enviados.
+		// Ao realizar o passo 2, foi possível obter os valores pelo String e imprimi-los no console.
+		// Ao realizar o passo 3, os valores continuaram os mesmos após simplificar o parâmetro.
 		
 		return "eventoAdicionado";
 	}

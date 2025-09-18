4739dd9df93d43e964ba1e1f201bf22d2a373432
Victor Kaell <victor.kaell@escolar.ifrn.edu.br>
2025-09-18 10:20:50 -0300

    Adicionar confirmação de formulário
    Foi adicionada uma confirmação no Controller para confirmar que o formulário foi enviado e redireciona o usuário até a página inicial.

--- src/main/java/ifrn/pi/inicio/controllers/EventosController.java
+++ src/main/java/ifrn/pi/inicio/controllers/EventosController.java
@@ -2,6 +2,7 @@ package ifrn.pi.inicio.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EventosController {
@@ -9,5 +10,12 @@ public class EventosController {
 	@GetMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
+	}
+	
+	@PostMapping("/eventos/adicionar")
+	public String adicionarEvento() {
+		System.out.println("Evento foi adicionado");
+		
+		return "eventoAdicionado";
 	}
 }

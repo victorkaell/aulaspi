5cad0b64f2edded09f7f2fc2659f05cc53b5db27
Victor Kaell <victorkaell13@gmail.com>
2025-09-30 17:28:24 -0300

    Adicionada listagem de eventos

--- src/main/java/ifrn/pi/inicio/controllers/IndexController.java
+++ src/main/java/ifrn/pi/inicio/controllers/IndexController.java
@@ -1,14 +1,28 @@
 package ifrn.pi.inicio.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import ifrn.pi.inicio.models.Evento;
+import ifrn.pi.inicio.repositories.EventoRepository;
 
 @Controller
 public class IndexController {
 	
+	@Autowired
+	private EventoRepository er;
+	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		List<Evento> eventos = er.findAll();
+		model.addAttribute("eventos", eventos);
+		
 		System.out.println("Chamou o método index");
+		
 		return "home";
 	}
 }
